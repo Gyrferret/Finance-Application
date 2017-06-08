6fa5fb36c434eeefc970f6ba70e8d92aa8f5f5cb
Gyrferret <dv@noloc.us>
2017-06-08 13:33:23 -0700

    Clean yo

--- Finance Application/Program.cs
+++ Finance Application/Program.cs
@@ -11,43 +11,38 @@ namespace Finance_Application
         static void Main(string[] args)
         {
             Processing Process = new Processing();
-            Console.WriteLine(Process.MaximumPrincipalAmount(550,12,1.095f));
+            PaymentInformation Information = new PaymentInformation();
+            double Constant = Process.DetermineConstant(36, 250000, 1.0395f);
+            double[,] Array = Information.MonthlyPayments(36, Constant, 250000, 1.0395f);
+            InformationOutput(Array);
             Console.ReadLine();
+        }
+
+        static void InformationOutput (double[,] InformationArray)
+        {
+            for (int i = 0; i < (InformationArray.Length / 4) ; i++)
+            {
+                Console.WriteLine("Total Due: {0}, Paid to Interest: {1}, Paid to Principal {2}, End Balance: {3}", InformationArray[i, 0], InformationArray[i, 1], InformationArray[i, 2], InformationArray[i, 3]);
+            }
         }
     }
     class Processing
     {
         public double DetermineConstant(int months = 36, float principal = 250000, float r = 1.0395f)
         {
+            // used to determine the constant monthly payment given a principal, constant months of payment, and constant rate.
             double BaseValue = 1;
             for (int i = 1; i < months; i++)
             {
                 BaseValue += Math.Pow(r, i);
             }
             double n = (principal * Math.Pow(r, months)) / BaseValue;
-            Console.WriteLine(n);
-            MonthlyPayments(n, months, principal, r);
             return n;
-        }
-
-        public void MonthlyPayments(double Constant, int months, float principal, float r)
-        {
-            double RunningTotal = principal;
-            double AccumulatedInterest;
-            double PrincipalPaid;
-            double InterestPaid;
-            for (int i = 0; i < months; i++)
-            {
-                AccumulatedInterest = (RunningTotal * r);
-                InterestPaid = (AccumulatedInterest - RunningTotal);
-                PrincipalPaid = Constant - InterestPaid;
-                Console.WriteLine("Beginning Balance: {0}, Interest Accrued: {1}, Paid to Principal: {2}, Paid to Interest {3}", AccumulatedInterest, InterestPaid, PrincipalPaid, InterestPaid);
-                RunningTotal = (AccumulatedInterest - Constant);
-            }
         }
 
         public double MaximumPrincipalAmount (double constant= 550, int months = 36 , float r = 1.095f)
         {
+            // used to determine the maximum principal allowed given a constant monthly payment, constant months of payment, and constant rate. 
             double BaseValue = constant;
             for (int i = 0; i < months; i++)
             {
